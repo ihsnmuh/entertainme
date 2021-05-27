68746f5a180517a5ee6c44d5acbcdf6bbfdb6c12
Muhammad Ihsan <muhammadihsan076@gmail.com>
2021-05-27 08:08:04 +0700

    Merge ke main

--- client/App.js
+++ client/App.js
@@ -17,7 +17,6 @@ import AddScreen from './src/screens/AddScreen/AddScreen';
 import AddMovieScreen from './src/screens/AddScreen/AddMovieScreen';
 import AddSeriesScreen from './src/screens/AddScreen/AddSeriesScreen';
 import EditScreen from './src/screens/EditScreen/EditScreen';
-import LoadingScreen from './src/screens/LoadingScreen';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
